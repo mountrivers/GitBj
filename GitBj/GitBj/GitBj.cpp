94bfff621daecf34e07c927f6379684c37787c17
sanhaLee <tksgk77@gmail.com>
2020-11-03 21:27:31 +0900

    완

--- GitBj/GitBj/GitBj.cpp
+++ GitBj/GitBj/GitBj.cpp
@@ -1,11 +1,33 @@
-// GitBj.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
+#include <iostream>
+#include <vector>
+#include <set>
 
-#include "stdafx.h"
-
+using namespace std;
 
 int main()
 {
+	int n;
+	int result = 5;
+	set<int> us;
+	cin >> n; 
+	
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		us.insert(temp);
+	}
+	for (auto iter = us.begin(); iter != us.end(); iter++) {
+		int temp = 4;
+		if (us.find(*iter + 5) == us.end()) {
+			for (int i = 1; i <= 4; i++) {
+				temp -= us.find(*iter + i) != us.end() ? 1 : 0;
+			}
+			if (result > temp) result = temp;
+		}
+	}
+
+	cout << result;
+
     return 0;
 }
 
